a118f2d78fcc42a5664e012f3f1890195d739863
Kyle <klknudson@crimson.ua.edu>
2023-04-06 20:01:35 -0500

    cal change

--- resources/scripts/cal.js
+++ resources/scripts/cal.js
@@ -1,8 +1,7 @@
 
 async function getVal(){
     const input = document.querySelector('input')
-    console.log(input.value)
     let date = new Date(input.value)
-    console.log(date.toUTCString())
+    console.log(date)
 }
 
