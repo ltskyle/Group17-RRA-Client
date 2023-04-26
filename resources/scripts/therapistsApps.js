07b844b5ebe3f7d73679737932d53a76af26e97a
Kyle <klknudson@crimson.ua.edu>
2023-04-26 11:41:30 -0500

    accept/decline

--- resources/scripts/therapistsApps.js
+++ resources/scripts/therapistsApps.js
@@ -25,32 +25,32 @@ const makeTable = (appointments) => {
     tableBody.appendChild(tr)
 
     let th1 = document.createElement('TH')
-    th1.width = 250
+    th1.width = 100
     th1.appendChild(document.createTextNode('Date'))
     tr.appendChild(th1)
 
     let th2 = document.createElement('TH')
-    th2.width = 250
+    th2.width = 120
     th2.appendChild(document.createTextNode('Time Frame'))
     tr.appendChild(th2)
 
     let th3 = document.createElement('TH')
-    th3.width = 100
+    th3.width = 250
     th3.appendChild(document.createTextNode('Type of Service'))
     tr.appendChild(th3)
 
     let th4 = document.createElement('TH')
-    th4.width = 100
+    th4.width = 200
     th4.appendChild(document.createTextNode('Client Name'))
     tr.appendChild(th4)
 
     let th5 = document.createElement('TH')
-    th5.width = 215
+    th5.width = 300
     th5.appendChild(document.createTextNode('Client Address'))
     tr.appendChild(th5)
 
     let th6 = document.createElement('TH')
-    th6.width = 215
+    th6.width = 230
     th6.appendChild(document.createTextNode('Status'))
     tr.appendChild(th6)
 
@@ -62,22 +62,22 @@ const makeTable = (appointments) => {
             tableBody.appendChild(tr)
 
             let td1 = document.createElement('TD')
-            td1.width = 250
+            td1.width = 100
             td1.appendChild(document.createTextNode(`${a.appointmentDate}`))
             tr.appendChild(td1)
 
             let td2 = document.createElement('TD')
-            td2.width = 250
+            td2.width = 120
             td2.appendChild(document.createTextNode(`${a.timeFrame}`))
             tr.appendChild(td2)
 
             let td3 = document.createElement('TD')
-            td3.width = 100
+            td3.width = 250
             td3.appendChild(document.createTextNode(`${a.typeOfService}`))
             tr.appendChild(td3)
 
             let td4 = document.createElement('TD')
-            td4.width = 100
+            td4.width = 200
             td4.appendChild(
                 document.createTextNode(
                     `${a.clientFirstName}` + ' ' + `${a.clientLastName}`
@@ -86,7 +86,7 @@ const makeTable = (appointments) => {
             tr.appendChild(td4)
 
             let td5 = document.createElement('TD')
-            td5.width = 100
+            td5.width = 300
             td5.appendChild(
                 document.createTextNode(
                     `${a.clientAddress}` +
@@ -102,63 +102,40 @@ const makeTable = (appointments) => {
 
             if (a.appointmentAcceptance == "false") {
                 let atbn = document.createElement('BUTTON')
-                atbn.className = 'btn-info'
+                atbn.className = 'myButton'
                 atbn.id = `${a.appointmentID}`
                 atbn.style = 'margin: 5px'
                 atbn.onclick = () => {
                     Accept(a.appointmentID)
                 }
-                atbn.width = 70
+                atbn.width = 100
                 atbn.appendChild(document.createTextNode('Accept'))
                 tr.appendChild(atbn)
 
                 let dtbn = document.createElement('BUTTON')
-                dtbn.className = 'btn-info'
+                dtbn.className = 'myButton'
                 dtbn.id = `${a.appointmentID}`
                 dtbn.style = 'margin: 5px'
                 dtbn.onclick = () => {
                     Decline(a.appointmentID)
                 }
-                dtbn.width = 70
+                dtbn.width = 100
                 dtbn.appendChild(document.createTextNode('Decline'))
                 tr.appendChild(dtbn)
             }
 
-            if (a.completed == 'false') {
+            else if (a.completed == 'false' && a.appointmentAcceptance) {
                 let ctbn = document.createElement('BUTTON')
-                ctbn.className = 'btn-info'
+                ctbn.className = 'myButton'
                 ctbn.id = `${a.appointmentID}`
                 ctbn.style = 'margin: 5px'
                 ctbn.onclick = () => {
                     Completed(a.appointmentID)
                 }
-                ctbn.width = 70
+                ctbn.width = 100
                 ctbn.appendChild(document.createTextNode('Complete'))
                 tr.appendChild(ctbn)
             }
-
-            // let dbtn = document.createElement('BUTTON')
-            // dbtn.className = 'btn-info'
-            // dbtn.id = `${s.songID}`
-            // dbtn.style = 'margin: 5px'
-            // dbtn.onclick = () => {
-            //     deleteSong(s.songID)
-            // }
-            // dbtn.width = 70
-            // dbtn.appendChild(document.createTextNode('Delete'))
-            // tr.appendChild(dbtn)
-
-            // let ebtn = document.createElement('BUTTON')
-            // ebtn.className = 'btn-info'
-            // ebtn.id = `${s.songID}`
-            // ebtn.style = 'margin: 5px'
-            // ebtn.onclick = () => {
-            //     $('#editSong').modal('show')
-            //     editSong(s.songID)
-            // }
-            // ebtn.width = 70
-            // ebtn.appendChild(document.createTextNode('Edit'))
-            // tr.appendChild(ebtn)
         }
     })
     app.appendChild(table)
@@ -218,7 +195,7 @@ async function Decline(ID) {
                 TypeOfService: finding.typeOfService,
                 Rating: finding.rating,
                 AppointmentAcceptance: 'NotAccepted',
-                Completed: finding.completed,
+                Completed: 'true',
                 AppointmentDate: finding.appointmentDate,
                 AppointmentCity: finding.appointmentCity,
                 AppointmentState: finding.appointmentState,
